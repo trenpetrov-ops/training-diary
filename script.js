0fe8bccfebe59cb5c2621220b56545648c4d9650
trenpetrov-ops <tren.petrov@gmail.com>
2025-11-04 16:09:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js"; 
 import {
     getAuth,
     onAuthStateChanged,
